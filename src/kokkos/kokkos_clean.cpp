c309277e441b56d7c58bd056545d70c009887fe6
bogumidu <bogumil.k.duda@gmail.com>
2022-09-29 19:03:26 +0200

    update gitignore, added test lines

--- src/kokkos/kokkos_clean.cpp
+++ src/kokkos/kokkos_clean.cpp
@@ -9,37 +9,57 @@
 
 struct hello_world {
 
+    int n;
+
+    KOKKOS_INLINE_FUNCTION
+    explicit
+    hello_world(int num) noexcept
+            : n(num)
+    { }
+
     // this is a macro that allows method to run on CUDA devices when using CUDA
     KOKKOS_INLINE_FUNCTION
     void operator()(const int i) const {
+        printf("N is %d, Hello from i = %i\n", n, i);
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        printf("Hello from i = %i\n", i);
     }
 };
 
 // Probably running on single core
 int main_kokkos(int argc, char *argv[]) {
-    auto voxels = FileMaker::loadFile::loadSchematic("test1.schematic");
-    for (const auto& voxel : voxels) {
-        std::cout << voxel.getX() << " " << voxel.getY() << " " << voxel.getZ() << " " << voxel.getColor() << std::endl;
+    std::cout << argc << ":\n";
+    std::string all_args[argc];
+    bool debug;
+
+    if (argc < 2) throw std::runtime_error("Missing object file name");
+
+
+    if (argc > 2) {
+        for (int i = 2; i <= argc - 1; i++) {
+            all_args[i] = argv[i];
+        }
     }
-    FileMaker::loadFile::saveSchematic("test2.schematic", voxels);
-//    char** kokkosArgs = new char*[argc + 1];
-//    for (int i = 0; i < argc; i++) {
-//        kokkosArgs[i] = argv[i];
+//    auto voxels = FileMaker::loadFile::loadSchematic("test1.schematic");
+//    for (const auto& voxel : voxels) {
+//        std::cout << voxel.getX() << " " << voxel.getY() << " " << voxel.getZ() << " " << voxel.getColor() << std::endl;
 //    }
-//    kokkosArgs[argc] = (char *) &"--kokkos-threads=3";
-//    int kokkosArgc = argc + 1;
-//    Kokkos::initialize(kokkosArgc, kokkosArgs);
-//
-//
-//    printf("Hello World on Kokkos execution space %s\n",
-//           typeid(Kokkos::DefaultExecutionSpace).name());
-//
-//    // executes function x amount of times
-//    Kokkos::parallel_for("HelloWorld", 15, hello_world());
-//
-//    Kokkos::finalize();
+//    FileMaker::loadFile::saveSchematic("test2.schematic", voxels);
+    char** kokkosArgs = new char*[argc + 1];
+    for (int i = 0; i < argc; i++) {
+        kokkosArgs[i] = argv[i];
+    }
+    kokkosArgs[argc] = (char *) &"";
+    int kokkosArgc = argc + 1;
+    Kokkos::initialize(kokkosArgc, kokkosArgs);
+
+
+    printf("Hello World on Kokkos execution space %s\n",
+           typeid(Kokkos::DefaultExecutionSpace).name());
+
+    // executes function x amount of times
+    Kokkos::parallel_for("HelloWorld", 15, hello_world(5));
+
+    Kokkos::finalize();
 
     return 0;
 }
